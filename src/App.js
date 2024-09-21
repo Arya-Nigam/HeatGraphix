449f365fff673f39a42719d083f21d463945c88f
Arya Nigam <135435014+Arya-Nigam@users.noreply.github.com>
2024-09-21 12:28:14 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+import Navbar from './components/Navbar';
+import Theory from './components/Theory'
+
+
+function App() {
+  return (
+    <>
+      <Navbar title="Navbar" />
+      <Theory/> 
+    </>
+  );
+}
+
+export default App;
