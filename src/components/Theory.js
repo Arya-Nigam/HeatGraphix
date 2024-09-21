f9f6e65d00c3dacff4e7762229cfb7b297da9e28
Arya Nigam <135435014+Arya-Nigam@users.noreply.github.com>
2024-09-21 12:30:01 +0530

    initial commit

---
+++ src/components/Theory.js
@@ -0,0 +1,103 @@
+import React from "react";
+
+export default function Theory() {
+  return (
+    <div>
+      <div>
+      <div className="shadow-lg p-4 mb-5 bg-body-tertiary rounded" >
+        HEAT TREATMENT PROCESSES
+        </div>
+        <div className="shadow-none p-3 mb-5 bg-body-tertiary rounded" className="text-secondary">
+        Heat treatment is a process of altering the physical properties of a material by heating and cooling it in a controlled manner. This can include processes like annealing, quenching, tempering, and carburizing, each with its specific purpose and techniques.
+        </div>
+
+        <div className="shadow p-3 mb-5 bg-body-tertiary rounded">
+        1. Annealing
+        </div>
+        <div className="shadow-none p-3 mb-3 bg-body-tertiary rounded" class="text-secondary">
+        Annealing is a heat treatment process that involves heating a material to a specific temperature, holding it there for a period of time, and then cooling it slowly. This process is used to alter the physical and sometimes chemical properties of the material, such as its hardness, ductility, and microstructure. By reducing internal stresses and refining the grain structure, annealing can improve the material's workability, strength, and resistance to fatigue.
+        </div>
+
+        <div className="shadow p-3 mb-5 bg-body-tertiary rounded">
+        2. Normalizing
+        </div>
+        <div className="shadow-none p-3 mb-3 bg-body-tertiary rounded" class="text-secondary">
+        Normalizing is a heat treatment process similar to annealing, but with a faster cooling rate. It involves heating the material to a temperature above the critical temperature, holding it there for a period of time, and then cooling it rapidly in air. This process is used to refine the grain structure of the material, improve its toughness, and reduce internal stresses. Normalizing is often used for materials that need to be strengthened and homogenized before further processing, such as forging or machining.
+        </div>
+
+        <div className="shadow p-3 mb-5 bg-body-tertiary rounded">
+        3. Quenching
+        </div>
+        <div className="shadow-none p-3 mb-3 bg-body-tertiary rounded" class="text-secondary">
+        Quenching is a heat treatment process that involves rapidly cooling a heated material, typically by immersing it in a liquid such as water, oil, or a salt bath. This rapid cooling process causes the material to harden and become brittle. Quenching is often used in combination with other heat treatment processes, such as tempering, to achieve specific properties like high hardness and wear resistance. The choice of quenching medium and cooling rate can significantly affect the resulting microstructure and properties of the material.
+        </div>
+
+        <div className="shadow p-3 mb-5 bg-body-tertiary rounded">
+        4. Tempering
+        </div>
+        <div className="shadow-none p-3 mb-3 bg-body-tertiary rounded" class="text-secondary">
+        Tempering is a heat treatment process that follows quenching. It involves reheating the quenched material to a temperature below its critical temperature, holding it there for a period of time, and then cooling it slowly. This process reduces the brittleness of the quenched material while retaining some of its hardness, resulting in a more ductile and tough material.
+        </div>
+      </div>
+
+      <div>
+      <div className="shadow-lg p-4 mb-5 bg-body-tertiary rounded" >
+        SURFACE HARDENING TECHNIQUES
+        </div>
+
+        <div className="shadow p-3 mb-5 bg-body-tertiary rounded">
+        1. Carburizing <br/>
+        i. Gas carburizing
+        </div>
+        <div className="shadow-none p-3 mb-3 bg-body-tertiary rounded" class="text-secondary">
+        Gas carburizing is a heat treatment process used to introduce carbon into the surface of a steel component. The steel is heated in a carbon-rich atmosphere, typically created by using gaseous hydrocarbons like methane or propane. This process results in a hard, wear-resistant surface layer while the core remains softer and more ductile.
+        </div>
+
+        <div className="shadow p-3 mb-5 bg-body-tertiary rounded">
+        ii. Annealing
+        </div>
+        <div className="shadow-none p-3 mb-3 bg-body-tertiary rounded" class="text-secondary">
+        Annealing is a heat treatment process that involves heating a material to a specific temperature, holding it there for a period of time, and then cooling it slowly. This process is used to alter the physical and sometimes chemical properties of the material, such as its hardness, ductility, and microstructure. By reducing internal stresses and refining the grain structure, annealing can improve the material's workability, strength, and resistance to fatigue.
+        </div>
+
+        <div className="shadow p-3 mb-5 bg-body-tertiary rounded">
+        iii. Annealing
+        </div>
+        <div className="shadow-none p-3 mb-3 bg-body-tertiary rounded" class="text-secondary">
+        Annealing is a heat treatment process that involves heating a material to a specific temperature, holding it there for a period of time, and then cooling it slowly. This process is used to alter the physical and sometimes chemical properties of the material, such as its hardness, ductility, and microstructure. By reducing internal stresses and refining the grain structure, annealing can improve the material's workability, strength, and resistance to fatigue.
+        </div>
+
+        <div className="shadow p-3 mb-5 bg-body-tertiary rounded">
+        2. Annealing
+        </div>
+        <div className="shadow-none p-3 mb-3 bg-body-tertiary rounded" class="text-secondary">
+        Annealing is a heat treatment process that involves heating a material to a specific temperature, holding it there for a period of time, and then cooling it slowly. This process is used to alter the physical and sometimes chemical properties of the material, such as its hardness, ductility, and microstructure. By reducing internal stresses and refining the grain structure, annealing can improve the material's workability, strength, and resistance to fatigue.
+        </div>
+
+        <div className="shadow p-3 mb-5 bg-body-tertiary rounded">
+        3. Annealing
+        </div>
+        <div className="shadow-none p-3 mb-3 bg-body-tertiary rounded" class="text-secondary">
+        Annealing is a heat treatment process that involves heating a material to a specific temperature, holding it there for a period of time, and then cooling it slowly. This process is used to alter the physical and sometimes chemical properties of the material, such as its hardness, ductility, and microstructure. By reducing internal stresses and refining the grain structure, annealing can improve the material's workability, strength, and resistance to fatigue.
+        </div>
+
+        <div className="shadow p-3 mb-5 bg-body-tertiary rounded">
+        4. Annealing
+        </div>
+        <div className="shadow-none p-3 mb-3 bg-body-tertiary rounded" class="text-secondary">
+        Annealing is a heat treatment process that involves heating a material to a specific temperature, holding it there for a period of time, and then cooling it slowly. This process is used to alter the physical and sometimes chemical properties of the material, such as its hardness, ductility, and microstructure. By reducing internal stresses and refining the grain structure, annealing can improve the material's workability, strength, and resistance to fatigue.
+        </div>
+
+      </div>
+
+      <div>
+      <div className="shadow-lg p-4 mb-5 bg-body-tertiary rounded" >
+        COOLING CURVES
+        </div>
+        <div className="shadow-none p-3 mb-3 bg-body-tertiary rounded" class="text-secondary">
+        Cooling curves are graphical representations of temperature changes over time during a cooling process. They are used to visualize the rate of cooling and identify critical points such as solidification temperatures, phase transformations, and the formation of different microstructures.
+        </div>
+      </div>
+    </div>
+  );
+}
